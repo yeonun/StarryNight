b9bf44ef7583fb08e1ce3481c8a3649ac075702d
yeonun <yekang.cau@gmail.com>
2019-10-28 19:58:44 +0900

    zodiac symbol add

--- app/src/main/java/com/example/snkids/ConstellationStoryActivity.java
+++ app/src/main/java/com/example/snkids/ConstellationStoryActivity.java
@@ -1,12 +1,26 @@
 package com.example.snkids;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ConstellationStoryActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constellationstory);
+
+
+        ImageButton btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
     }
 }
